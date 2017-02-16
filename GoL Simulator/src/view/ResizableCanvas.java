bcb10d260849dc51570648d49e635b1975bba315
Aleksander Helgaker <aleksander.helgaker@protonmail.com>
2017-02-16 11:53:44 +0100

    Code cleanup

--- GoL Simulator/src/view/ResizableCanvas.java
+++ GoL Simulator/src/view/ResizableCanvas.java
@@ -6,8 +6,10 @@ import javafx.scene.paint.Color;
 import model.Board;
 
 /**
- * Provides same functionality as a regular canvas, but allows for fluid resizing.
- * To draw on the canvas use the draw method and initialized GraphicsContext2D. 
+ * Provides same functionality as a regular canvas, but allows for fluid
+ * resizing. To draw on the canvas use the draw method and initialized
+ * GraphicsContext2D.
+ *
  * @author peven
  */
 public class ResizableCanvas extends Canvas {
@@ -19,12 +21,12 @@ public class ResizableCanvas extends Canvas {
     private GraphicsContext gc;
     private int spaceBetweenCells;
 
-
     /**
-     * Default constructor. Needed by FXML loader to initialize canvas properly. Don't use this.
+     * Default constructor. Needed by FXML loader to initialize canvas properly.
+     * Don't use this.
      */
-    public ResizableCanvas (){
-        super(800d,800d);
+    public ResizableCanvas() {
+        super(800d, 800d);
         gc = this.getGraphicsContext2D();
         gc = this.getGraphicsContext2D();
         backGroundColor = Color.GREY;
@@ -33,34 +35,36 @@ public class ResizableCanvas extends Canvas {
         cellSize = 10;
         spaceBetweenCells = 2;
     }
+
     /**
      * Provides the ability to draw a grid of cells, both living and dead.
+     *
      * @param b Board class containing a two dimensional byte array.
      */
     public void draw(Board b) {
-        this.heightProperty().setValue((b.getHeight()+2)*(cellSize+spaceBetweenCells));
-        this.widthProperty().setValue((b.getWidth()+2)*(cellSize+spaceBetweenCells));
+        this.heightProperty().setValue((b.getHeight() + 2) * (cellSize + spaceBetweenCells));
+        this.widthProperty().setValue((b.getWidth() + 2) * (cellSize + spaceBetweenCells));
 
-        for (int row = 0; row < b.getBoard().length; row++) {     
+        for (int row = 0; row < b.getBoard().length; row++) {
             for (int col = 0; col < b.getBoard()[0].length; col++) {
                 if (b.getBoard()[row][col] == 1) {
                     gc.setFill(livingCellColor);
                 } else {
                     gc.setFill(deadCellColor);
                 }
-                gc.fillRect((col*(cellSize+spaceBetweenCells)), (row*(cellSize+spaceBetweenCells)), cellSize, cellSize);
+                gc.fillRect((col * (cellSize + spaceBetweenCells)), (row * (cellSize + spaceBetweenCells)), cellSize, cellSize);
             }
         }
-        
+
     }
-    
-        public int cellClickedRow(double x) {
-            return (int) (x/(cellSize+spaceBetweenCells));
-        }
-        
-        public int cellClickedCol(double y) {
-            return (int) (y/(cellSize+spaceBetweenCells));
-        }
+
+    public int cellClickedRow(double x) {
+        return (int) (x / (cellSize + spaceBetweenCells));
+    }
+
+    public int cellClickedCol(double y) {
+        return (int) (y / (cellSize + spaceBetweenCells));
+    }
 
     /**
      *
@@ -93,6 +97,7 @@ public class ResizableCanvas extends Canvas {
     public void setDeadCellColor(Color newCellColor) {
         this.deadCellColor = newCellColor;
     }
+
     @Override
     public boolean isResizable() {
         return true;
