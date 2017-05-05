8cbccee8e62211a0d959982c563cd08a4643fe1f
Petter Even Westby Solheim <s305466@stud.hioa.no>
2017-05-05 15:11:03 +0200

    fix

--- GoL Simulator/src/model/SimilarityMeasure.java
+++ GoL Simulator/src/model/SimilarityMeasure.java
@@ -1,23 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
+/**
+ * This is a wrapper class for sending a similarity measure between methods and objects.
+ * It is used both in Statistics.java and StatisticsWindowController.java.
+ */
 public class SimilarityMeasure {
 
     private int generation, similairtyValue;
 
+    /**
+     * Constructor
+     * @param mostSimilarGeneration the most similar generation of the measure.
+     * @param similarityValue the similarity value
+     */
     public SimilarityMeasure(int mostSimilarGeneration, int similarityValue) {
         this.generation = mostSimilarGeneration;
         this.similairtyValue = similarityValue;
     }
 
+    /**
+     *
+     * @return <ocde>int</code> the most similar generation
+     */
     public int getGeneration() {
         return generation;
     }
 
+    /**
+     *
+     * @return <code>int</code> the similarity value.
+     */
     public int getSimilairtyValue() {
         return similairtyValue;
     }
